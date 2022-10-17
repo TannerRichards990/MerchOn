9463721ede5485c6f9f1de980ff04bf35c113d8b
Connor McCue <connor.mccue@comcast.net>
2022-10-17 16:49:39 -0700

    storefront and filterstorefront setup

---
+++ src/Components/Storefront/FilterStorefront.js
@@ -0,0 +1,5 @@
+export default function FilterStorefront() {
+  return (
+    <div>FilterStorefront</div>
+  );
+}
--- src/Components/Storefront/Storefront.js
+++ src/Components/Storefront/Storefront.js
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import { signOut } from '../../services/auth';
 
 
@@ -8,7 +9,11 @@ export default function Storefront() {
       <h1>congrats you made it here!</h1>
       <div>
         <button onClick={signOut}>Sign Out</button>
+        <NavLink to="/Landing/">Landing</NavLink>
       </div>
+      <main>
+        
+      </main>
     </>
   );
 }
