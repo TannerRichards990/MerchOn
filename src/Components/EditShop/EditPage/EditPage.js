5c45ef46e5aac04a050035609f039a79094d7b53
Tanner Richards <Tanner.richards990@gmail.com>
2022-10-18 11:16:02 -0700

    merge to main

---
+++ src/Components/EditShop/EditPage/EditPage.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+import Preview from '../Preview/Preview';
+import EditShop from '../Editor/EditShop';
+
+import './EditPage.css';
+
+export default function EditPage() {
+  const [shopName, setShopName] = useState('');
+  const [aboutShop, setAboutShop] = useState('');
+  const [shopFont, setShopFont] = useState('default');
+  const [shopTheme, setShopTheme] = useState('default');
+  const [shopImage, setShopImage] = useState('');
+  const [addItem, setAddItem] = useState([]);
+
+
+  return (
+    <div>
+      <Preview {...{ shopName, aboutShop, shopImage, shopTheme, shopFont, addItem }} />
+      <EditShop {...
+        { shopName, 
+          setShopName, 
+          aboutShop, 
+          setAboutShop, 
+          shopImage, 
+          setShopImage, 
+          shopTheme, 
+          setShopTheme, 
+          shopFont, 
+          setShopFont, 
+          addItem, 
+          setAddItem }} />
+    </div>
+  );
+}
+
