5eba52c7debc770b1e660d68cf510f08f136fe6a
Emily Pisaygnane <episaygnane@me.com>
2022-10-17 17:08:45 -0700

    added tanners info

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,6 +4,8 @@ import ConnorLinked from '../../assets/ConnorLinked.png';
 import ConnorGit from '../../assets/ConnorGit.png';
 import EmilyLinked from '../../assets/EmilyLinked.png';
 import EmilyGit from '../../assets/EmilyGit.png';
+import TannerLinked from '../../TannerLinked.png';
+import TannerGit from '../../TannerGit.png';
 
 export default function About() {
   return (
@@ -17,31 +19,31 @@ export default function About() {
           <h3>Connor McCue</h3>
           <p>LinkedIn</p>
           <a href="https://www.linkedin.com/in/connor-mccue/" target="_blank" rel="noreferrer">
-            <img src={ConnorLinked}></img>
+            <img src={ ConnorLinked }></img>
           </a>
           <p>GitHub</p>
           <a href="https://github.com/csmccue" target="_blank" rel="noreferrer">
-            <img src={ConnorGit}></img>
+            <img src={ ConnorGit }></img>
           </a>
 
           <h3>Emily Pisaygnane</h3>
           <p>LinkedIn</p>
-          <a href="https://www.linkedin.com/in/emily-pisaygnane/"target="_blank" rel="noreferrer">
-            <img src={EmilyLinked}></img>
+          <a href="https://www.linkedin.com/in/emily-pisaygnane/" target="_blank" rel="noreferrer">
+            <img src={ EmilyLinked }></img>
           </a>
           <p>GitHub</p>
-          <a href="https://github.com/emilypisaygnane"target="_blank" rel="noreferrer">
-            <img src={EmilyGit}></img>
+          <a href="https://github.com/emilypisaygnane" target="_blank" rel="noreferrer">
+            <img src={ EmilyGit }></img>
           </a>
 
           <h3>Tanner Richards</h3>
           <p>LinkedIn</p>
-          <a href=""target="_blank" rel="noreferrer">
-            <img src={}></img>
+          <a href="https://www.linkedin.com/in/tannerrichards/" target="_blank" rel="noreferrer">
+            <img src={ TannerLinked }></img>
           </a>
           <p>GitHub</p>
-          <a href=""target="_blank" rel="noreferrer">
-            <img src={}></img>
+          <a href="https://github.com/TannerRichards990" target="_blank" rel="noreferrer">
+            <img src={ TannerGit }></img>
           </a>
 
           <h3>Thomas P. Jenkins</h3>
