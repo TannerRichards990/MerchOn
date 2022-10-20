e44d4359bea958efe52b2195e0fe7f407c885895
Connor McCue <connor.mccue@comcast.net>
2022-10-20 10:55:39 -0700

    cart subtotal

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -3,24 +3,30 @@ import CartItem from './CartItem';
 
 export default function Cart() {
   let totalSum = 0;
-  console.log(localStorage);
+  let totalSumString = '';
+  // console.log(localStorage);
   let shoppingCart = JSON.parse(localStorage.getItem('cart'));
-  console.log('shopping cart', shoppingCart);
+  // console.log('shopping cart', shoppingCart);
 
   const clearCart = () => {
     localStorage.clear();
     console.log(localStorage);
   };
 
-  const totalMoney = () => {
+  function totalMoney() {
     let moneyArr = [];
     shoppingCart.map((item) => (
       moneyArr.push(item[1])
     ));
-    console.log('mon array', moneyArr);
-    moneyArr.reduce((a, b) => a + b, 0);
-    console.log('possible sum', moneyArr);
-  };
+    moneyArr.map((number) => (
+      totalSum = totalSum + Number(number)
+    ));
+    totalSum = (Math.round(totalSum * 100) / 100);
+    totalSumString = totalSum.toString();
+    console.log(totalSum);
+  }
+
+  totalMoney();
 
   return (
     <>    
@@ -31,15 +37,19 @@ export default function Cart() {
             <div className='cart'>
               {shoppingCart.map((item) => (
                 <div key={item} {...item}>
-                  <div>This should show the item name: {item[0]}</div>
-                  <div>This should show the item price: {item[1]}</div>
+                  <li>
+                    This should show the item name: {item[0]}
+                    <h1>
+                    This should show the item price: {item[1]}
+                    </h1>
+                  </li>
                 </div>
               ))}
             </div>
             <button onClick={clearCart}>Clear Cart</button>
             <button onClick={totalMoney}>find sum</button>
-            <div>
-                  sum: {totalSum}
+            <div className='subtotal'>
+                  Subtotal: ${totalSumString}
             </div>
           </div>
         )}
