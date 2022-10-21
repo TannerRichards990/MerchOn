6d8a90c0a85b495de21e86b25936cd83f8eff547
Emily Pisaygnane <episaygnane@me.com>
2022-10-20 17:23:18 -0700

    fixed one small teeny tiny error on useUser.js

--- src/Hooks/useUser.js
+++ src/Hooks/useUser.js
@@ -1,4 +1,4 @@
-import { useContext useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../Context/UserContext';
 import { grabID } from '../services/fetch-utils';
 
